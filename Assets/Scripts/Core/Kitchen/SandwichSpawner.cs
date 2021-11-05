114057d06a715035c34efecb022c1d604ac4e24c
sergey.binder <fornewreal@gmail.com>
2021-11-05 22:20:21 +0700

    Kitchen balance tweaks

--- Assets/Scripts/Core/Kitchen/SandwichSpawner.cs
+++ Assets/Scripts/Core/Kitchen/SandwichSpawner.cs
@@ -1,9 +1,12 @@
 ﻿using UnityEngine;
+using UnityEngine.Assertions;
 
 namespace SGJ.Core.Kitchen {
 	public sealed class SandwichSpawner : MonoBehaviour {
 		[Header("Parameters")]
 		public float SpawnDelay;
+		public float MinSandwichGravityScale = 0.5f;
+		public float MaxSandwichGravityScale = 0.8f;
 		[Header("Dependencies")]
 		public GameObject SandwichPrefab;
 		public Transform StartSpawnPoint;
@@ -25,8 +28,11 @@ namespace SGJ.Core.Kitchen {
 		}
 
 		void Spawn() {
-			var pos = Vector3.Lerp(StartSpawnPoint.position, EndSpawnPoint.position, Random.Range(0f, 1f));
-			Instantiate(SandwichPrefab, pos, Quaternion.identity);
+			var pos        = Vector3.Lerp(StartSpawnPoint.position, EndSpawnPoint.position, Random.Range(0f, 1f));
+			var sandwichGo = Instantiate(SandwichPrefab, pos, Quaternion.identity);
+			var sandwichRb = sandwichGo.GetComponent<Rigidbody2D>();
+			Assert.IsTrue(sandwichRb);
+			sandwichRb.gravityScale = Random.Range(MinSandwichGravityScale, MaxSandwichGravityScale);
 		}
 	}
 }
