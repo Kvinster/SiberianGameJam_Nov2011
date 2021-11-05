ec4ff36696e9c8a85d7d0ff73ce24a211b5ed8f6
sergey.binder <fornewreal@gmail.com>
2021-11-05 18:08:48 +0700

    Added version text

--- Assets/Scripts/MainMenu/MainScreen.cs
+++ Assets/Scripts/MainMenu/MainScreen.cs
@@ -2,12 +2,16 @@
 using UnityEngine.SceneManagement;
 using UnityEngine.UI;
 
+using TMPro;
+
 namespace SGJ.MainScreen {
 	public sealed class MainScreen : MonoBehaviour {
-		public Button StartGameButton;
+		public Button   StartGameButton;
+		public TMP_Text VersionText;
 
 		void Start() {
 			StartGameButton.onClick.AddListener(OnStartGameClick);
+			VersionText.text = $"Version: {Application.version}";
 		}
 
 		void OnStartGameClick() {
