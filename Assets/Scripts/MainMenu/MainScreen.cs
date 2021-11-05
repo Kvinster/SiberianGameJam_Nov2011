7b6dcfe97fbea83ad1333ac56fe589fa53025af2
sergey.binder <fornewreal@gmail.com>
2021-11-05 18:13:40 +0700

    Added exit button

--- Assets/Scripts/MainMenu/MainScreen.cs
+++ Assets/Scripts/MainMenu/MainScreen.cs
@@ -7,15 +7,25 @@ using TMPro;
 namespace SGJ.MainScreen {
 	public sealed class MainScreen : MonoBehaviour {
 		public Button   StartGameButton;
+		public Button   ExitButton;
 		public TMP_Text VersionText;
 
 		void Start() {
 			StartGameButton.onClick.AddListener(OnStartGameClick);
+			ExitButton.onClick.AddListener(OnExitClick);
 			VersionText.text = $"Version: {Application.version}";
 		}
 
 		void OnStartGameClick() {
 			SceneManager.LoadScene("Meta");
+		}
+
+		void OnExitClick() {
+#if UNITY_EDITOR
+			UnityEditor.EditorApplication.isPlaying = false;
+#else
+			Application.Quit();
+#endif
 		}
 	}
 }
