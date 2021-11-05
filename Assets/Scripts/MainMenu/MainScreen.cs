932a272fad54c3ad3cebf7b4f9fb9a2c10a05339
sergey.binder <fornewreal@gmail.com>
2021-11-05 15:18:15 +0700

    Draft entry point scene

---
+++ Assets/Scripts/MainMenu/MainScreen.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using UnityEngine.UI;
+
+namespace SGJ.MainScreen {
+	public sealed class MainScreen : MonoBehaviour {
+		public Button StartGameButton;
+
+		void Start() {
+			StartGameButton.onClick.AddListener(OnStartGameClick);
+		}
+
+		void OnStartGameClick() {
+			// TODO: change scene, remove debug log
+			Debug.Log("Start game");
+		}
+	}
+}
