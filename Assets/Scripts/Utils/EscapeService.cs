13e1e6ac0b5b6cafb0319e6a6e741d41e4a94bba
sergey.binder <fornewreal@gmail.com>
2021-11-05 21:38:04 +0700

    Added escape service

---
+++ Assets/Scripts/Utils/EscapeService.cs
@@ -0,0 +1,29 @@
+﻿using UnityEngine;
+
+namespace SGJ.Utils {
+	public sealed class EscapeService : MonoBehaviour {
+		static EscapeService _instance;
+
+		void OnEnable() {
+			if ( !_instance ) {
+				_instance = this;
+			} else {
+				Destroy(gameObject);
+			}
+		}
+
+		void Start() {
+			DontDestroyOnLoad(gameObject);
+		}
+
+		void Update() {
+			if ( Input.GetKeyDown(KeyCode.Escape) ) {
+#if UNITY_EDITOR
+				UnityEditor.EditorApplication.isPlaying = false;
+#else
+				Application.Quit();
+#endif
+			}
+		}
+	}
+}
