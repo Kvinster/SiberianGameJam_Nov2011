01dd6e4011ba9501538a16a62d76a1d3d5381131
sergey.binder <fornewreal@gmail.com>
2021-11-07 22:13:41 +0700

    Added Kitchen steps sound

--- Assets/Scripts/Core/Kitchen/KitchenCutscenePlayer.cs
+++ Assets/Scripts/Core/Kitchen/KitchenCutscenePlayer.cs
@@ -4,6 +4,8 @@ using SGJ.Utils;
 
 using DG.Tweening;
 
+using SGJ.Service;
+
 namespace SGJ.Core.Kitchen {
 	public sealed class KitchenCutscenePlayer : MonoBehaviour {
 		[Header("Parameters")]
@@ -20,6 +22,8 @@ namespace SGJ.Core.Kitchen {
 		public AlphaGroup Cutscene0AlphaGroup;
 		public AlphaGroup Cutscene1AlphaGroup;
 		public AlphaGroup GameplayAlphaGroup;
+		[Space]
+		public AudioClip StepsClip;
 
 		void Start() {
 			Play();
@@ -34,10 +38,12 @@ namespace SGJ.Core.Kitchen {
 
 			DOTween.Sequence()
 				.AppendInterval(PlayTime)
+				.AppendCallback(() => AudioService.PlayInPool(this, StepsClip, true))
 				.Append(LadyTransform.DOMove(LadyEndPos.position, PlayTime).SetEase(Ease.InSine))
 				.Join(LadyTransform.DORotate(LadyEndPos.rotation.eulerAngles, PlayTime))
 				.Append(DOTween.To(() => Cutscene0AlphaGroup.Alpha, x => Cutscene0AlphaGroup.Alpha = x, 0f, FadeTime))
 				.Join(DOTween.To(() => Cutscene1AlphaGroup.Alpha, x => Cutscene1AlphaGroup.Alpha   = x, 1f, FadeTime))
+				.Join(DOTween.To(() => AudioService.GetVolumeInPool(this), x => AudioService.SetVolumeInPool(this, x), 0f, 0.2f))
 				.AppendInterval(PlayTime)
 				.Append(DOTween.To(() => Cutscene1AlphaGroup.Alpha, x => Cutscene1AlphaGroup.Alpha = x, 0f, FadeTime))
 				.Join(DOTween.To(() => GameplayAlphaGroup.Alpha, x => GameplayAlphaGroup.Alpha   = x, 1f, FadeTime))
