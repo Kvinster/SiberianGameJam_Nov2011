7151f5874df8b45f38bea7454a20fb53ef287fb9
sergey.binder <fornewreal@gmail.com>
2021-11-07 21:54:22 +0700

    Soap sound

---
+++ Assets/Scripts/Core/Bathroom/SoapSoundPlayer.cs
@@ -0,0 +1,24 @@
+﻿using UnityEngine;
+
+using SGJ.Service;
+
+namespace SGJ.Core.Bathroom {
+	public sealed class SoapSoundPlayer : MonoBehaviour {
+		public Soap      Soap;
+		public AudioClip Clip;
+
+		void OnEnable() {
+			Soap.OnMoved += OnSoapMoved;
+		}
+
+		void OnDisable() {
+			if ( Soap ) {
+				Soap.OnMoved -= OnSoapMoved;
+			}
+		}
+
+		void OnSoapMoved(SideType obj) {
+			AudioService.PlaySound(Clip);
+		}
+	}
+}
