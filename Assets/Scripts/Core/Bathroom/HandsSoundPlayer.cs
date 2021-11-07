98e5c9bb1a21678b5c08386a1b2dcf38e474a86d
sergey.binder <fornewreal@gmail.com>
2021-11-07 22:02:21 +0700

    Hands sounds

---
+++ Assets/Scripts/Core/Bathroom/HandsSoundPlayer.cs
@@ -0,0 +1,32 @@
+﻿using UnityEngine;
+
+using SGJ.Service;
+
+namespace SGJ.Core.Bathroom {
+	public sealed class HandsSoundPlayer : MonoBehaviour {
+		public Hand[]    Hands;
+		public AudioClip GrabbedClip;
+		public AudioClip MissedClip;
+
+		void OnEnable() {
+			foreach ( var hand in Hands ) {
+				hand.OnGrabbedSoap += OnHandGrabbedSoap;
+				hand.OnMissedSoap += OnHandMissedSoap;
+			}
+		}
+
+		void OnDisable() {
+			AudioService.StopInPool(this);
+		}
+
+		void OnHandMissedSoap(Hand obj) {
+			AudioService.StopInPool(this);
+			AudioService.PlayInPool(this, MissedClip, false);
+		}
+
+		void OnHandGrabbedSoap(Hand obj) {
+			AudioService.StopInPool(this);
+			AudioService.PlayInPool(this, GrabbedClip, false);
+		}
+	}
+}
