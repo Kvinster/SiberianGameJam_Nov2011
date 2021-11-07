f8a3dcc5bc54677ffd3709059cb554c53e1d39c7
sergey.binder <fornewreal@gmail.com>
2021-11-07 17:23:48 +0700

    Showing cheats only after the keys combination is pressed
    The combination is Space + 1 + 2 + 3

--- Assets/Scripts/MainMenu/MainScreen.cs
+++ Assets/Scripts/MainMenu/MainScreen.cs
@@ -13,10 +13,19 @@ namespace SGJ.MainScreen {
 		public Button   ExitButton;
 		public TMP_Text VersionText;
 
+		bool _showCheats;
+
 		void Start() {
 			StartGameButton.onClick.AddListener(OnStartGameClick);
 			ExitButton.onClick.AddListener(OnExitClick);
 			VersionText.text = $"Version: {Application.version}";
+		}
+
+		void Update() {
+			if ( Input.GetKey(KeyCode.Space) && Input.GetKey(KeyCode.Alpha1) && Input.GetKey(KeyCode.Alpha2) &&
+			     Input.GetKey(KeyCode.Alpha3) ) {
+				_showCheats = true;
+			}
 		}
 
 		void OnStartGameClick() {
@@ -31,16 +40,18 @@ namespace SGJ.MainScreen {
 #endif
 		}
 
-		void OnGUI() { // TODO: disable cheats in production
-			GUILayout.Space(10);
-			for ( var i = 0; i < LevelController.RoomsOrder.Length; i++ ) {
-				var roomType = LevelController.RoomsOrder[i];
-				if ( GUILayout.Button(roomType.ToString(), new GUIStyle(GUI.skin.button) {
-					fontSize = 20 * Mathf.RoundToInt(Screen.height / 1080f)
-				}, GUILayout.MinWidth(Screen.width / 16f), GUILayout.MinHeight(Screen.height / 18f)) ) {
-					LevelController.Instance.CheatSetNextLevelIndex(i);
-					SceneManager.LoadScene(RoomTypeHelper.RoomTypeToSceneName(roomType));
-					return;
+		void OnGUI() {
+			if ( _showCheats ) {
+				GUILayout.Space(10);
+				for ( var i = 0; i < LevelController.RoomsOrder.Length; i++ ) {
+					var roomType = LevelController.RoomsOrder[i];
+					if ( GUILayout.Button(roomType.ToString(),
+						new GUIStyle(GUI.skin.button) { fontSize = 20 * Mathf.RoundToInt(Screen.height / 1080f) },
+						GUILayout.MinWidth(Screen.width / 16f), GUILayout.MinHeight(Screen.height / 18f)) ) {
+						LevelController.Instance.CheatSetNextLevelIndex(i);
+						SceneManager.LoadScene(RoomTypeHelper.RoomTypeToSceneName(roomType));
+						return;
+					}
 				}
 			}
 		}
